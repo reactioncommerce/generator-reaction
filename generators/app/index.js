5631001915a2ff989da346eb78e8c0e1d900b2af
Ticean Bennett <ticean@gmail.com>
2018-02-12 13:04:23 -0800

    Progress toward base project generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,21 +1,66 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the excellent ' + chalk.red('generator-reaction-next') + ' generator!'
-    ));
+    this.log(
+      yosay(
+        "Welcome to the excellent " +
+          chalk.red("generator-reaction-next") +
+          " generator!"
+      )
+    );
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    const prompts = [
+      {
+        default: "reaction-demo",
+        message: "Your project name.",
+        name: "projectName",
+        type: "input"
+      },
+      {
+        default: "0.1.0",
+        message: "Version",
+        name: "projectVersion",
+        type: "input"
+      },
+      {
+        message: "Description",
+        name: "projectDescription",
+        type: "input"
+      },
+      {
+        message: "Keywords",
+        name: "projectKeywords",
+        type: "input"
+      },
+      {
+        message: "Author",
+        name: "projectAuthor",
+        store: true,
+        type: "input"
+      },
+      {
+        message: "Author Email",
+        name: "projectAuthorEmail",
+        store: true,
+        type: "input"
+      },
+      {
+        message: "Keep NPM repository private?",
+        name: "isNPMPrivate",
+        type: "confirm"
+      },
+      {
+        default: true,
+        message: "Enable GraphQL?",
+        name: "includeGraphQL",
+        type: "confirm"
+      }
+    ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
@@ -24,13 +69,21 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    // package.json
+    this.fs.copyTpl(
+      this.templatePath("package.json"),
+      this.destinationPath("package.json"),
+      this.props
     );
   }
 
   install() {
-    this.installDependencies();
+    // We'll not install dependencies because this should be via Docker build.
+  }
+
+  end() {
+    this.log(chalk.bold.green("\nGenerator setup finished."));
+    this.log("If you see no errors above, run the server with Docker Compose:");
+    this.log(chalk.bold.white("docker-compose up"));
   }
 };
