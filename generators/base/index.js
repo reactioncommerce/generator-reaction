c075b213466ef259dd8e46af4422e9fec7449b8a
Ticean Bennett <ticean@gmail.com>
2018-07-02 11:14:11 -0700

    chore: Update default authorship email
    Use the hello@ address - same as on the Reaction project.

--- generators/base/index.js
+++ generators/base/index.js
@@ -29,7 +29,7 @@ module.exports = class extends Generator {
         type: 'input'
       },
       {
-        default: 'engineering@reactioncommerce.com',
+        default: 'hello@reactioncommerce.com',
         message: 'Author Email',
         name: 'projectAuthorEmail',
         type: 'input'
